d23a46d27794f263450741117de3cd814b9da68d
THEKINGZONES <72527649+THEKINGZONES@users.noreply.github.com>
2020-12-12 06:59:55 -0700

    Last version

--- src/Business/YueBusiness.cs
+++ src/Business/YueBusiness.cs
@@ -9,26 +9,27 @@ namespace Business
     public class YueBusiness
     {
         DA.Yue _YueConexion;
-        DA.ConexionIBM _Conexion;
+        DA.ConexionIBM _Conexion;        
 
         public YueBusiness()
         {
-            _YueConexion = new DA.Yue();
-            _Conexion = new DA.ConexionIBM("Eco2");            
-
+            _YueConexion = new DA.Yue();           
+            _Conexion = new DA.ConexionIBM("Eco2");
+           
         }
         
         public YueGroupStation GetYueGroupStation(string GroupStation)
         {
-            return _YueConexion.GetGroupStation(GroupStation);
+            return _YueConexion.GetGroupStation(GroupStation); 
         }
 
         public DataTable GetStationsInStations(string BeginStation,string EndStation)
         {
-            var GrupA = _YueConexion.GetGroupStation(BeginStation);
-            var GrupB = _YueConexion.GetGroupStation(EndStation);
+            var GrupA = _YueConexion.GetGroupStation(BeginStation);        
+
             var query = _YueConexion.GetReport(DA.enumYueReport.STATIONSINSTATIONS);
             query = query.Replace("@StationsA", GrupA.GetListString);
+            var GrupB = _YueConexion.GetGroupStation(EndStation);
             query = query.Replace("@StationsB", GrupB.GetListString);
 
             var result = _Conexion.GetDataTable(query);
